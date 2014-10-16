6f5487d4f822c48e43fc5525f1b5f00e96c9afd8
Shashi Gowda <shashigowda91@gmail.com>
2014-10-16 17:25:43 +0530

    Now we just pack the built version and download it for the host image

---
+++ build.sh
@@ -0,0 +1,10 @@
+cd /tmp/ &&
+    git clone https://github.com/shashi/codebox.git &&
+    cd codebox &&
+    git checkout stable &&
+    npm install . &&
+    node_modules/grunt-cli/bin/grunt &&
+    bin/codebox.js &&
+    rm -rf .git &&
+    echo "At http://localhost:3000/ login using the username and password test1 and test1. This will complete the setup. Then create a tar ball of this directory" &&
+    x-www-browser http://localhost:3000/?email=test1&token=test1
