3f4a80d600c50e811e0bdd27fd4d48636d801220
Shashi Gowda <shashigowda91@gmail.com>
2014-11-12 00:36:19 +0530

    tiny workarounds

--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@ cd /tmp/ &&
     git checkout stable &&
     npm install . &&
     node_modules/grunt-cli/bin/grunt &&
-    bin/codebox.js
+    bin/codebox.js &&
+    echo "At http://localhost:3000/ login using the username and password test1 and test1. This will complete the setup. Then create a tar ball of this directory" &&
+    x-www-browser http://localhost:3000/?email=test1&token=test1
